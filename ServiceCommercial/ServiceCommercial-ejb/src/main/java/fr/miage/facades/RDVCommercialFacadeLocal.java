e05b0a7296cbbc2dc2259c3e1ef0b2d42d4f798e
Hazilys <camille.briosne@hotmail.fr>
2022-01-03 18:22:55 +0100

    Création des façades pour ServiceCo et SCA

---
+++ ServiceCommercial/ServiceCommercial-ejb/src/main/java/fr/miage/facades/RDVCommercialFacadeLocal.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.miage.facades;
+
+import fr.miage.entities.RDVCommercial;
+import java.util.List;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author Camille
+ */
+@Local
+public interface RDVCommercialFacadeLocal {
+
+    void create(RDVCommercial rDVCommercial);
+
+    void edit(RDVCommercial rDVCommercial);
+
+    void remove(RDVCommercial rDVCommercial);
+
+    RDVCommercial find(Object id);
+
+    List<RDVCommercial> findAll();
+
+    List<RDVCommercial> findRange(int[] range);
+
+    int count();
+    
+}
