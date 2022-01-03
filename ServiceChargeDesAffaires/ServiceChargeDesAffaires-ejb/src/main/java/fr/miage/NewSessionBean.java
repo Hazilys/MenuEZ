e7aa12094f0ecad6e7598da290596e8e11e74789
Hazilys <camille.briosne@hotmail.fr>
2022-01-03 17:28:14 +0100

    Réparation des entités SCA

--- ServiceChargeDesAffaires/ServiceChargeDesAffaires-ejb/src/main/java/fr/miage/NewSessionBean.java
+++ ServiceChargeDesAffaires/ServiceChargeDesAffaires-ejb/src/main/java/fr/miage/NewSessionBean.java
@@ -1,21 +1,24 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package fr.miage.entities;
+package fr.miage;
 
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
 
 /**
  *
- * @author Camille
+ * @author martin
  */
 @Stateless
 @LocalBean
 public class NewSessionBean {
 
+    public void businessMethod() {
+    }
+
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
+
 }
