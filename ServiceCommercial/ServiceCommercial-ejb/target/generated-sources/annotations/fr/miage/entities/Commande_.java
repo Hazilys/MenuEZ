db9e081ae3030b5486a4402154b7aeef7242be4d
Hazilys <camille.briosne@hotmail.fr>
2022-01-03 17:52:50 +0100

    Ajout entités ServiceCommercial

---
+++ ServiceCommercial/ServiceCommercial-ejb/target/generated-sources/annotations/fr/miage/entities/Commande_.java
@@ -0,0 +1,19 @@
+package fr.miage.entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-01-03T17:49:31")
+@StaticMetamodel(Commande.class)
+public class Commande_ { 
+
+    public static volatile SingularAttribute<Commande, String> cotes;
+    public static volatile SingularAttribute<Commande, Boolean> commandePassee;
+    public static volatile SingularAttribute<Commande, Long> idAffaire;
+    public static volatile SingularAttribute<Commande, String> refCatalogue;
+    public static volatile SingularAttribute<Commande, Double> montantNegocie;
+    public static volatile SingularAttribute<Commande, Boolean> commandeLivree;
+    public static volatile SingularAttribute<Commande, Long> idCommande;
+
+}
---
+++ ServiceCommercial/ServiceCommercial-ejb/target/generated-sources/annotations/fr/miage/entities/RDVCommercial_.java
@@ -0,0 +1,18 @@
+package fr.miage.entities;
+
+import java.util.Calendar;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-01-03T17:49:31")
+@StaticMetamodel(RDVCommercial.class)
+public class RDVCommercial_ { 
+
+    public static volatile SingularAttribute<RDVCommercial, Calendar> dateRDVCommercial;
+    public static volatile SingularAttribute<RDVCommercial, Long> idAffaire;
+    public static volatile SingularAttribute<RDVCommercial, Long> idClient;
+    public static volatile SingularAttribute<RDVCommercial, Long> idCommercial;
+    public static volatile SingularAttribute<RDVCommercial, Long> id;
+
+}
