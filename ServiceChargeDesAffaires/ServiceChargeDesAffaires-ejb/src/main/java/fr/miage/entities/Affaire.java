0c5596a638926f8b6065574e009c1345952ef2b1
Hazilys <camille.briosne@hotmail.fr>
2022-01-03 15:39:05 +0100

    Création des entités pour ServiceChargeDesAffaires (non fonctionnel)

---
+++ ServiceChargeDesAffaires/ServiceChargeDesAffaires-ejb/src/main/java/fr/miage/entities/Affaire.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.miage.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+/**
+ *
+ * @author Camille
+ */
+@Entity
+public class Affaire implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long idAffaire;
+    
+    @NotNull
+    private String geolocalisation;
+    
+    @NotNull
+    private ETAT etat;
+    
+    @NotNull
+    private Long idCA;
+    
+    @ManyToOne
+    private Client client;
+
+    public Affaire() {
+    }
+
+    public Long getIdAffaire() {
+        return idAffaire;
+    }
+
+    public void setIdAffaire(Long idAffaire) {
+        this.idAffaire = idAffaire;
+    }
+
+    public String getGeolocalisation() {
+        return geolocalisation;
+    }
+
+    public void setGeolocalisation(String geolocalisation) {
+        this.geolocalisation = geolocalisation;
+    }
+
+    public ETAT getEtat() {
+        return etat;
+    }
+
+    public void setEtat(ETAT etat) {
+        this.etat = etat;
+    }
+
+    public Long getIdCA() {
+        return idCA;
+    }
+
+    public void setIdCA(Long idCA) {
+        this.idCA = idCA;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idAffaire != null ? idAffaire.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the idAffaire fields are not set
+        if (!(object instanceof Affaire)) {
+            return false;
+        }
+        Affaire other = (Affaire) object;
+        if ((this.idAffaire == null && other.idAffaire != null) || (this.idAffaire != null && !this.idAffaire.equals(other.idAffaire))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "fr.miage.entities.Affaire[ id=" + idAffaire + " ]";
+    }
+    
+}
---
+++ ServiceChargeDesAffaires/ServiceChargeDesAffaires-ejb/src/main/java/fr/miage/entities/Client.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.miage.entities;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+
+/**
+ *
+ * @author Camille
+ */
+@Entity
+public class Client implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long idClient; 
+    
+    @NotNull
+    private String adressePostale;
+    
+    @NotNull
+    private String mail;
+    
+    @NotNull
+    private String telephone;
+    
+    @OneToMany(mappedBy = "client")
+    private Collection<Affaire> affaires;
+
+    public Client() {
+    }    
+    
+    public Long getIdClient() {
+        return idClient;
+    }
+
+    public void setIdClient(Long idClient) {
+        this.idClient = idClient;
+    }
+
+    public String getAdressePostale() {
+        return adressePostale;
+    }
+
+    public void setAdressePostale(String adressePostale) {
+        this.adressePostale = adressePostale;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public Collection<Affaire> getAffaires() {
+        return affaires;
+    }
+
+    public void setAffaires(Collection<Affaire> affaires) {
+        this.affaires = affaires;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idClient != null ? idClient.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the idClient fields are not set
+        if (!(object instanceof Client)) {
+            return false;
+        }
+        Client other = (Client) object;
+        if ((this.idClient == null && other.idClient != null) || (this.idClient != null && !this.idClient.equals(other.idClient))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "fr.miage.entities.Client[ id=" + idClient + " ]";
+    }
+    
+}
