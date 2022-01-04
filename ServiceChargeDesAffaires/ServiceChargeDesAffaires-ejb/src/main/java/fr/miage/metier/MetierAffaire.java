7015af4bb1f1bcbfbf3e3f6e7cae274c1c66ab89
Hazilys <camille.briosne@hotmail.fr>
2022-01-04 21:04:42 +0100

    Ajout du métier SCA

--- ServiceChargeDesAffaires/ServiceChargeDesAffaires-ejb/src/main/java/fr/miage/metier/MetierAffaire.java
+++ ServiceChargeDesAffaires/ServiceChargeDesAffaires-ejb/src/main/java/fr/miage/metier/MetierAffaire.java
@@ -5,16 +5,11 @@
  */
 package fr.miage.metier;
 
-<<<<<<< Updated upstream
-import fr.miage.entities.Client;
-import fr.miage.entities.ETAT;
-=======
 import fr.miage.entities.Affaire;
 import fr.miage.entities.Client;
 import fr.miage.entities.ETAT;
 import fr.miage.facades.AffaireFacadeLocal;
 import javax.ejb.EJB;
->>>>>>> Stashed changes
 import javax.ejb.Stateless;
 
 /**
@@ -24,18 +19,12 @@ import javax.ejb.Stateless;
 @Stateless
 public class MetierAffaire implements MetierAffaireLocal {
 
-<<<<<<< Updated upstream
-    @Override
-    public void creerAffaire(String geolocalisation, ETAT etat, Long idCa, Client client) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-=======
     @EJB
     private AffaireFacadeLocal affaireFacade;
 
     @Override
     public void creerAffaire(String geolocalisation, Long idCa, Client client) {
         this.affaireFacade.creerAffaire(geolocalisation, idCa, client);
->>>>>>> Stashed changes
     }
 
     // Add business logic below. (Right-click in editor and choose
